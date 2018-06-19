7a0394efc207e1513c1b1c17d37e068fa3c18d9f
ARYUNAP <AMI619847@GMAIL.com>
2018-06-19 13:44:38 +0200

    added mainscreen folder and its components

---
+++ src/components/mainscreen/MainscreenContainer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import UsernameForm from './UsernameForm';
+import PasswordForm from './PasswordForm';
+import LoginButton from './LoginButton';
+import RegisterButton from './RegisterButton';
+
+export default function Mainscreen (props) {
+  return (
+
+    <div className="mainscreen">
+      <header className="App-header">
+        <h1>LOGO</h1>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      </header>
+      <UsernameForm />
+      <PasswordForm />
+      {/* <Link to="/hangman">Start Playing!</Link> */}
+      <br/>
+      <LoginButton />
+
+      <RegisterButton />
+    </div>
+  )
+}
